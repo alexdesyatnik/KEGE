c1c29b260538a54f7510a8409d02fec4738d9c25
Alexey Desyatnik <alexey.desyatnik@yandex.ru>
2023-12-27 10:22:18 +0300

    Упростил решение задачи 12254

--- 24/24_12254.py
+++ 24/24_12254.py
@@ -1,25 +1,20 @@
 s = open("./24/24_12254.txt").readline().strip()
 
-maxlen = 1
-
-begin = 0
-end = 0
-
-
 # RSQ: R->S->Q->R
-
 correct_next = {
     "R": "S",
     "S": "Q",
     "Q": "R",
 }
 
-while end < len(s) - 1:
-    if s[end + 1] == correct_next[s[end]]:
-        end += 1        
+maxlen = 1
+currlen = 1
+
+for i in range(1, len(s)):
+    if s[i] == correct_next[s[i - 1]]:
+        currlen += 1
+        maxlen = max(maxlen, currlen)
     else:
-        begin = end + 1
-        end = begin
-    maxlen = max(maxlen, end - begin + 1)
+        currlen = 1
 
 print(maxlen)
