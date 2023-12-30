6fe1ef477a1b3e175b1d97f3de550640e5c04e2a
Alexey Desyatnik <alexey.desyatnik@yandex.ru>
2023-12-30 10:07:21 +0300

    Немного упростил решение 24_8654

--- 24/24_8654.py
+++ 24/24_8654.py
@@ -1,18 +1,15 @@
 # Определите максимальное количество идущих подряд символов, среди которых нет
 # ни одной подстроки, соответствующей маске ?B??D
-
-from fnmatch import fnmatch
-
 s = open("24_8654.txt").readline()
 
 begin = end = 0
 maxlen = 1
 
-while end < len(s):
+while end < len(s) - 1:
     if end % 10000 == 0:
         print(".", end="")
     end += 1
-    if fnmatch(s[end - 4 : end + 1], "?B??D"):
+    if s[end] == "D" and s[end - 3] == "B":
         begin = end - 3
     maxlen = max(maxlen, end - begin + 1)
 
