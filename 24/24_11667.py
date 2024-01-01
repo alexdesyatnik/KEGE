9f8e39868c0f55ab31b27ad9c6bc3a792f57135b
Alexey Desyatnik <alexey.desyatnik@yandex.ru>
2024-01-01 13:26:10 +0300

    Решил №24 11667 (компегэ)

---
+++ 24/24_11667.py
@@ -0,0 +1,26 @@
+s = open("./24_11667.txt").readline().strip()
+
+"""
+Определите в прилагаемом файле максимальное количество идущих подряд символов
+(длину непрерывной подпоследовательности), среди которых сочетание символов
+INFINITY встречается ровно 1000 раз.
+"""
+
+begin = end = 0
+maxlen = 0
+inf_count = 0
+
+while end < len(s) - 1:
+    end += 1
+    if end % 100000 == 0:
+        print(".", end="")
+    if s[end - 7 : end + 1] == "INFINITY":
+        inf_count += 1
+    if inf_count == 1000:
+        maxlen = max(maxlen, end - begin + 1)
+    elif inf_count > 1000:
+        begin = s.find("INFINITY", begin) + 1
+        inf_count -= 1
+
+print()
+print(maxlen)
