ee74ebdc43254ba0c2485780e89e606898271e5c
Alexey Desyatnik <alexey.desyatnik@yandex.ru>
2024-01-09 20:57:31 +0300

    Упростил решение 7853

--- 24/24_7853.py
+++ 24/24_7853.py
@@ -2,12 +2,9 @@ s = open("./24_7853.txt").readline().strip()
 
 maxlen = b = e = 0
 
-for letter in "NOT":
-    s = s.replace(letter, "*")
-
 while e < len(s):
     e += 1
-    if s[e - 1] == "*" and s[e - 3] == "*":
+    if s[e - 1] in "NOT" and s[e - 3] in "NOT":
         b = e - 2
     maxlen = max(maxlen, e - b)
 
