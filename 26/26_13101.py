e44f08c5b4dd84f7daf44fdf472c4c6c490972a4
Alexey Desyatnik <alexey.desyatnik@yandex.ru>
2024-05-10 18:53:35 +0300

    Немного улучшил решение тип 26 №13101 (компегэ)

--- 26/26_13101.py
+++ 26/26_13101.py
@@ -3,20 +3,19 @@ n = int(f.readline())
 clients = [[int(x) for x in line.split()] for line in f]
 clients.sort()
 
-windows = [[], []]
+windows = [None, [], []]
 window2_count = 0
 left_count = 0
 
 for time, duration, win_num in clients:
-    windows[0] = [end_time for end_time in windows[0] if end_time > time]
     windows[1] = [end_time for end_time in windows[1] if end_time > time]
+    windows[2] = [end_time for end_time in windows[2] if end_time > time]
     if win_num == 0:
-        win_num = 1 if len(windows[0]) <= len(windows[1]) else 2
-    win_num -= 1
+        win_num = 1 if len(windows[1]) <= len(windows[2]) else 2
     if len(windows[win_num]) >= 14:
         left_count += 1
     else:
-        if win_num == 1:
+        if win_num == 2:
             window2_count += 1
         if len(windows[win_num]) == 0:
             windows[win_num].append(time + duration)
